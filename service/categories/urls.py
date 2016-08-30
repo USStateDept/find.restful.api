527146e7ba910b965e806d2183eba2fd169b4786
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-08-29 21:18:38 -0400

    Working categories requests - might need to refine the queries

--- service/categories/urls.py
+++ service/categories/urls.py
@@ -4,13 +4,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (
     CategoryListAPIView,
     CategoryDetailByIdAPIView,
-    CategoryDetailAPIView
+    CategoryDetailAPIView,
+    SubcategoryDetailAPIView
     )
 
 urlpatterns = [
     url(r'^$', CategoryListAPIView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', CategoryDetailByIdAPIView.as_view(), name='id'),
-    url(r'^(?P<category_name>[\w-]+)/$', CategoryDetailAPIView.as_view(), name='detail'),
+    url(r'^category/(?P<category_name>[\w, ]+)/$', CategoryDetailAPIView.as_view(), name='category_detail'),
+    url(r'^subcategory/(?P<sub_category_name>[\w, ]+)/$', SubcategoryDetailAPIView.as_view(), name='sub_category_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
