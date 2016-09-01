ab94fb54b1d223372f0044a9df582ddd42103dbf
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-09-01 10:46:26 -0400

    Removed the settings file. And added more stuff.

--- service/region_data/models.py
+++ service/region_data/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class RegionData(models.Model):
+    region_data = models.AutoField(db_column='Region_Data', primary_key=True)  # Field name made lowercase.
+    indicator_id = models.IntegerField(db_column='Indicator_ID', blank=True, null=True)  # Field name made lowercase.
+    region_id = models.IntegerField(db_column='Region_ID', blank=True, null=True)  # Field name made lowercase.
+    year = models.IntegerField(db_column='Year', blank=True, null=True)  # Field name made lowercase.
+    avg = models.FloatField(db_column='Avg', blank=True, null=True)  # Field name made lowercase.
+    createdat = models.DateTimeField(db_column='createdAt')  # Field name made lowercase.
+    updatedat = models.DateTimeField(db_column='updatedAt')  # Field name made lowercase.
+    # dataviewdataviewid = models.ForeignKey(DataViews, models.DO_NOTHING, db_column='DataViewDataViewID', blank=True, null=True)  # Field name made lowercase.
+    # regiondatumregiondata = models.ForeignKey('self', models.DO_NOTHING, db_column='RegionDatumRegionData', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        # managed = False
+        db_table = 'Region_Data'
--- service/regions/models.py
+++ service/regions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Regions(models.Model):
+    region_id = models.AutoField(db_column='Region_ID', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    type = models.CharField(db_column='Type', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        # managed = False
+        db_table = 'Regions'
